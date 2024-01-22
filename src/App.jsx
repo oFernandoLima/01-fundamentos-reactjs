43c3a475818e7759cc9484beb327a464e3c0d4bb
Fernando Lima <eufernandoprogramador@gmail.com>
2024-01-22 18:11:38 -0300

    feat: Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import "global.css"
+import { Header } from "./components/Header"
+import "./global.css"
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </div>
   )
 }
 
